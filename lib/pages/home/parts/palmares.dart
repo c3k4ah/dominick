e7bd98eebaa683589e95c8e53fef8749bbfd5174
c3k4ah <dominickp20.aps1b@gmail>
2023-05-15 00:50:56 +0300

    [ADD] parcours widget

--- lib/pages/home/parts/palmares.dart
+++ lib/pages/home/parts/palmares.dart
@@ -5,7 +5,7 @@ import '../../../common/colors/colors.dart';
 import '../../../common/utils/sizes/pad_margin.dart';
 import 'widgets/my_palmares_widget.dart';
 
-class MyPalmaresPart extends StatelessWidget {
+class MyPalmaresPart extends StatefulWidget {
   final Size size;
   const MyPalmaresPart({
     super.key,
@@ -13,27 +13,90 @@ class MyPalmaresPart extends StatelessWidget {
   });
 
   @override
+  State<MyPalmaresPart> createState() => _MyPalmaresPartState();
+}
+
+class _MyPalmaresPartState extends State<MyPalmaresPart> {
+  int selectedIndex = 100;
+  @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(
         horizontal: bodyPadding,
         vertical: 20,
       ),
-      height: size.height * .9,
-      width: size.width,
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 2,
-          color: whiteColor,
-        ),
-      ),
+      height: widget.size.height * .9,
+      width: widget.size.width,
       child: Column(
         children: [
           Container(
-            width: size.width,
-            height: 100,
-            margin: const EdgeInsets.symmetric(vertical: 20),
-            child: const Placeholder(),
+            width: widget.size.width,
+            height: 150,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Column(
+              children: [
+                Text(
+                  'Parcours',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: whiteColor,
+                    fontSize: 50,
+                  ),
+                ),
+                Container(
+                  width: 350,
+                  height: 70,
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(
+                    color: secondaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 150,
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: whiteColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Experiences',
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: secondaryColor,
+                              fontSize: 20,
+                              fontFamily: 'Product Sans',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: secondaryColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Formations',
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: whiteColor,
+                              fontSize: 20,
+                              fontFamily: 'Product Sans',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
           Expanded(
             child: ScrollChild(
@@ -41,8 +104,21 @@ class MyPalmaresPart extends StatelessWidget {
                 itemCount: 5,
                 scrollDirection: Axis.vertical,
                 itemBuilder: (context, index) {
-                  return MyPalmaresWidget(
-                    size: size,
+                  return MouseRegion(
+                    onHover: (event) {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                    onExit: (event) {
+                      setState(() {
+                        selectedIndex = 100;
+                      });
+                    },
+                    child: MyPalmaresWidget(
+                      isSelected: index == selectedIndex,
+                      size: widget.size,
+                    ),
                   );
                 },
               ),
--- lib/pages/home/parts/widgets/my_palmares_widget.dart
+++ lib/pages/home/parts/widgets/my_palmares_widget.dart
@@ -1,22 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:unicons/unicons.dart';
 
 import '../../../../common/colors/colors.dart';
 
 class MyPalmaresWidget extends StatelessWidget {
   final Size size;
+  final bool isSelected;
   const MyPalmaresWidget({
     super.key,
     required this.size,
+    required this.isSelected,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: size.width,
-      height: 80,
-      color: whiteColor,
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: const Placeholder(),
+      height: 120,
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      decoration: isSelected
+          ? BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(10),
+            )
+          : BoxDecoration(
+              color: primaryColor,
+              border: Border(
+                top: BorderSide(
+                  width: 2,
+                  color: secondaryColor,
+                ),
+              ),
+            ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 5,
+              ).copyWith(left: 50),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '2023',
+                    style: TextStyle(
+                      color: isSelected ? secondaryColor : whiteColor,
+                      fontSize: 20,
+                      fontFamily: 'Product Sans',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Container(
+                    width: 70,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: isSelected ? secondaryColor : tertioColor,
+                        width: 5,
+                      ),
+                      image: const DecorationImage(
+                        image: AssetImage('/images/sample.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Stage d'apprentisage",
+                    style: TextStyle(
+                      color: isSelected ? secondaryColor : whiteColor,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 80,
+                    width: 160,
+                    child: Row(
+                      children: [
+                        Icon(
+                          UniconsLine.location_point,
+                          color: isSelected ? secondaryColor : tertioColor,
+                        ),
+                        Text(
+                          "Ankatso",
+                          style: TextStyle(
+                            color: isSelected ? secondaryColor : whiteColor,
+                            fontFamily: 'Product Sans',
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 80,
+                    width: 200,
+                    child: Row(
+                      children: [
+                        Icon(
+                          UniconsLine.code_branch,
+                          color: isSelected ? secondaryColor : tertioColor,
+                        ),
+                        Text(
+                          "Dev mobile",
+                          style: TextStyle(
+                            color: isSelected ? secondaryColor : whiteColor,
+                            fontFamily: 'Product Sans',
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: 100,
+            decoration: BoxDecoration(
+                color: isSelected ? tertioColor : primaryColor,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                )),
+            child: Center(
+              child: Icon(
+                UniconsLine.angle_right_b,
+                size: 30,
+                color: isSelected ? secondaryColor : whiteColor,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
