e50d874a5f04ca2056789ece0fe734deaa4ea5a9
c3k4ah <dominickp20.aps1b@gmail.com>
2024-04-23 01:24:10 +0300

    Update image asset paths in image_assets.dart and pubspec.yaml

--- lib/data/image_assets.dart
+++ lib/data/image_assets.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 final ImageAssets imageAsset = ImageAssets();
 
 class ImageAssets {
-  String directory = 'assets/assets'; //!(kIsWeb) ? 'assets/' : '';
+  String directory = 'assets/'; //!(kIsWeb) ? 'assets/' : '';
 
   //logo
   String get inonia => '${directory}logo/ibonia.png';
@@ -29,9 +29,49 @@ class ImageAssets {
   String get buildeo => '${directory}images/buildeo.png';
   String get avg => '${directory}images/avg.png';
   //icon
-  String get team => '${directory}icons/team.svg';
-  String get project => '${directory}icons/project.svg';
-  String get cup => '${directory}icons/cup.svg';
-  String get calendar => '${directory}icons/calendar.svg';
-  String get mouse => '${directory}icons/mouse_icon.svg';
+  String get team => '${directory}icon/team.svg';
+  String get project => '${directory}icon/project.svg';
+  String get cup => '${directory}icon/cup.svg';
+  String get calendar => '${directory}icon/calendar.svg';
+  String get mouse => '${directory}icon/mouse_icon.svg';
 }
+
+/**
+  assets:
+    - assets/
+    - assets/images/
+    - assets/icon/
+    - assets/logo/
+    - assets/images/angry.webp
+    - assets/images/sample.png
+    - assets/icon/team.svg
+    - assets/icon/project.svg
+    - assets/icon/cup.svg
+    - assets/icon/calendar.svg
+    - assets/icon/mouse_icon.svg
+    - assets/logo/ibonia.png
+    - assets/logo/areaTech.png
+    - assets/logo/myagency.png
+    - assets/logo/google.png
+    - assets/logo/esti.png
+    - assets/logo/mybn.png
+    - assets/logo/mvola.png
+    - assets/logo/zahageek.png
+    - assets/logo/iteams.png
+    - assets/images/cekah.png
+    - assets/images/cher_project.jpg
+    - assets/images/designer_project.jpeg
+    - assets/images/dev_project.jpg
+    - assets/images/dekoa.png
+    - assets/images/mozika.png
+    - assets/images/rakitra.png
+    - assets/images/gfs.png
+    - assets/images/buildeo.png
+    - assets/images/avg.png
+    - assets/icons/team.svg
+    - assets/icons/project.svg
+    - assets/icons/cup.svg
+    - assets/icons/calendar.svg
+    - assets/icons/mouse_icon.svg
+
+ */
