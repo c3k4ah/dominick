d00a9c1917623fe3c65a0e7cf439dde11412140f
c3k4ah <dominickp20.aps1b@gmail.com>
2024-05-01 06:41:30 +0300

    Update devtools_options.yaml, netlify.toml, pubspec.lock, pubspec.yaml, web/index.html, and web/zoom.js

--- web/zoom.js
+++
@@ -1,9 +0,0 @@
-function setZoom(zoomLevel) {
-    document.body.style.zoom = zoomLevel;
-}
-function getScreenSize() {
-    return {
-    width: window.screen.width,
-    height: window.screen.height
-    };
-}
